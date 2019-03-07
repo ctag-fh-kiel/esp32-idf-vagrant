3d249f34dfa742cf8d6cfb59e761a7049737aa5a
Robert Manzke <ctag-fh-kiel@users.noreply.github.com>
2019-03-07 17:05:46 +0100

    Update 0_install-toolchain.sh

--- 0_install-toolchain.sh
+++ 0_install-toolchain.sh
@@ -1,61 +1,70 @@
-#!/usr/bin/env bash
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
 
-# update system
-apt-get update
-apt-get upgrade -y
-# setup ttyusb
-# https://askubuntu.com/questions/935350/ubuntu-16-04-1-usbserial-missing/984031
-apt-get install -y linux-image-extra-virtual wget unzip tar
+Vagrant.configure(2) do |config|
+  # The most common configuration options are documented and commented below.
+  # For a complete reference, please see the online documentation at
+  # https://docs.vagrantup.com.
 
-# Install basic development tools
-#sudo apt-get install -y libusb-1.0-0 libusb-1.0-0-dev libftdi1 libftdi-dev 
+  # Every Vagrant development environment requires a box. You can search for
+  # boxes at https://atlas.hashicorp.com/search.
+  config.vm.box = "ubuntu/xenial64"
 
-# Install basic development tools
-apt-get install -y git make libncurses-dev flex bison gperf python python-serial python-pip python-dev build-essentialgcc g++ linux-headers-$(uname -r)
+  # Disable automatic box update checking. If you disable this, then
+  # boxes will only be checked for updates when the user runs
+  # `vagrant box outdated`. This is not recommended.
+  # config.vm.box_check_update = false
 
-# Allow non-root users to access USB devices
-echo 'SUBSYSTEMS=="usb", ATTRS{idVendor}=="10C4", ATTRS{idProduct}=="EA60", GROUP="users", MODE="0666"' >> /etc/udev/rules.d/60-programmers.rules
-echo 'KERNEL=="ttyUSB0", MODE="0666"' >> /etc/udev/rules.d/60-programmers.rules
-sudo udevadm control --reload-rules
-sudo udevadm trigger
+  # Create a forwarded port mapping which allows access to a specific port
+  # within the machine from a port on the host machine. In the example below,
+  # accessing "localhost:8080" will access port 80 on the guest machine.
+  # config.vm.network "forwarded_port", guest: 80, host: 8080
 
-# Install toolchain for esp32 and set up environment variables
-cd /home/vagrant
+  # Create a private network, which allows host-only access to the machine
+  # using a specific IP.
+  # config.vm.network "private_network", ip: "192.168.33.10"
 
-wget -nv https://dl.espressif.com/dl/xtensa-esp32-elf-linux64-1.22.0-80-g6c4433a-5.2.0.tar.gz
-tar -xzf xtensa-esp32-elf-linux64-1.22.0-80-g6c4433a-5.2.0.tar.gz
-echo "export PATH=\"$PATH:/home/vagrant/xtensa-esp32-elf/bin:/home/vagrant/esp32ulp-elf-binutils/bin\"" >> .bash_profile 
-git clone --recursive https://github.com/espressif/esp-idf.git
-echo "export IDF_PATH=/home/vagrant/esp-idf" >> .bash_profile
-pip install --upgrade pip
-python -m pip install --user -r /home/vagrant/esp-idf/requirements.txt
-cd /home/vagrant/esp-idf
-git checkout release/v3.2
-cd /home/vagrant
-wget -nv https://github.com/espressif/binutils-esp32ulp/releases/download/v2.28.51-esp32ulp-20180809/binutils-esp32ulp-linux64-2.28.51-esp32ulp-20180809.tar.gz
-tar xf binutils-esp32ulp-linux64-2.28.51-esp32ulp-20180809.tar.gz
+  # Create a public network, which generally matched to bridged network.
+  # Bridged networks make the machine appear as another physical device on
+  # your network.
+  # config.vm.network "public_network"
 
-# create esp dir
-mkdir host/esp
-cp -r esp-idf/examples/wifi/scan host/esp
-chown vagrant:vagrant -R /home/vagrant
+  # Share an additional folder to the guest VM. The first argument is
+  # the path on the host to the actual folder. The second argument is
+  # the path on the guest to mount the folder. And the optional third
+  # argument is a set of non-required options.
+  config.vm.synced_folder ".", "/home/vagrant/host"
 
-# node.js v8
-cd ~
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt-get install -y nodejs
-
-# German layouts
-# switch to German keyboard layout in gnome desktop
-dbus-launch --exit-with-session gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'de'), ('xkb', 'us')]"
-
-# switch to German keyboard layout for console
-export DEBIAN_FRONTEND=noninteractive
-sed -i 's/"us"/"de"/g' /etc/default/keyboard
-apt-get install -y console-common
-install-keymap de
-
-# set timezone to German timezone
-echo 'Europe/Zurich' > /etc/timezone 
-rm /etc/localtime
-dpkg-reconfigure -f noninteractive tzdata
+  # Provider-specific configuration so you can fine-tune various
+  # backing providers for Vagrant. These expose provider-specific options.
+  # Example for VirtualBox:
+  #
+  config.vm.provider "virtualbox" do |vb|
+   # Display the VirtualBox GUI when booting the machine
+   # comment this if you want a vm with no GUI
+   vb.gui = true
+   
+   # Name of VM
+   vb.name = "esp32-idf-3.2-mar19"
+   # Customize the amount of memory on the VM:
+   vb.memory = "2048"
+   
+   # Use 2 CPUs
+   vb.cpus = 2
+        
+   # Enable all USB devices needed for development
+   vb.customize ['modifyvm', :id, '--usb', 'on']
+   vb.customize ['modifyvm', :id, '--usbehci', 'on']
+   vb.customize ['usbfilter', 'add', '0', '--target', :id, '--name', 'Silicon Labs CP2102 USB to UART Bridge Controller', '--vendorid', '0x10C4', '--productid', '0xEA60']
+   #vb.customize ['usbfilter', 'add', '0', '--target', :id, '--name', 'Olimex OpenOCD JTAG ARM-USB-TINY-H', '--vendorid', '0x15BA', '--productid', '0x002a']
+   #vb.customize ['usbfilter', 'add', '0', '--target', :id, '--name', 'Olimex OpenOCD JTAG ARM-USB-OCD-H', '--vendorid', '0x15BA', '--productid', '0x002b']
+   #vb.customize ['usbfilter', 'add', '0', '--target', :id, '--name', 'Olimex OpenOCD JTAG', '--vendorid', '0x15BA', '--productid', '0x0003']
+   #vb.customize ['usbfilter', 'add', '0', '--target', :id, '--name', 'AVRISP mkII', '--vendorid', '0x03EB', '--productid', '0x2104']
+   #vb.customize ['usbfilter', 'add', '0', '--target', :id, '--name', 'FTDI serial adapter', '--vendorid', '0x0403', '--productid', '0x6001']
+  end
+ 
+  # provisioning scripts
+  config.vm.provision :shell, path: "0_install-toolchain.sh"
+  # comment this if you want a vm with no GUI
+  config.vm.provision :shell, path: "1_install_x.sh"
+end
